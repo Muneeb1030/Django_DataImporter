6cc6b3c6dd085882c288e5816a57580c700e601c
Muneeb <m.muneeb.ur.rehman.2000@gmail.com>
2024-06-13 15:28:50 +0500

    Import data form Created

---
+++ importer/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path("import-data", views.importdata, name="importdata"),
+]
--- importer/views.py
+++ importer/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def importdata(request):
+    return render(request, 'importer/importdata.html')
